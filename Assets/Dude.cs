6ab8cb4a4a3ca66f8ed492df091022987411f9fb
talford2 <talford2@hotmail.com>
2014-09-24 22:42:53 +1000

    Initial stuff

---
+++ Assets/Dude.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dude : MonoBehaviour
+{
+    Animator animatorThing;
+
+    // Use this for initialization
+    void Start()
+    {
+        animatorThing = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animatorThing.SetFloat("Speed", Input.GetAxis("Vertical"));
+
+        //if (Input.GetAxis("Vertical") > 0)
+        //{ 
+        //}
+        //if (Input.GetAxis("Vertical") < 0)
+        //{
+        //}
+    }
+}
