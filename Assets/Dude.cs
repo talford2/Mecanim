b9a83a038c2ae518d572f07047aba1096414af44
Steven Alford <steven.alford@collinsonlatitude.com>
2014-09-24 21:54:12 +0800

    Get relative aim yaw value.

--- Assets/Dude.cs
+++ Assets/Dude.cs
@@ -30,10 +30,26 @@ public class Dude : MonoBehaviour
         animatorThing.SetFloat("Speed", moving.normalized.magnitude);
 
         var targetYaw = Mathf.Atan2(facing.x, facing.y)*Mathf.Rad2Deg;
-        Debug.Log(moving + " - " + targetYaw);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, targetYaw, transform.eulerAngles.z), turnSpeed*Time.deltaTime);
+
+        var aimAtPosition = GetScreenPointInWorldPlane(Input.mousePosition, 1.0f);
+        var toAimPosition = aimAtPosition - transform.position;
+        var aimYaw = Quaternion.LookRotation(toAimPosition).eulerAngles.y;
+        var relativeAimYaw = aimYaw - transform.eulerAngles.y;
+
+        Debug.Log(transform.eulerAngles.y + " | " + relativeAimYaw);
 
         ChaseCamera.transform.position = transform.position + new Vector3(0, 7.5f, -7.5f);
         ChaseCamera.transform.LookAt(transform.position);
+    }
+
+    private Vector3 GetScreenPointInWorldPlane(Vector3 screenPoint, float height)
+    {
+        var ray = Camera.main.ScreenPointToRay(screenPoint);
+        var worldPlane = new Plane();
+        var dist = 0f;
+        worldPlane.SetNormalAndPosition(Vector3.up, new Vector3(0, height, 0));
+        worldPlane.Raycast(ray, out dist);
+        return ray.GetPoint(dist);
     }
 }
