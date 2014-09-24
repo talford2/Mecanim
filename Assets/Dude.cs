4a0550eefb14216f09424c1d19a3bdd063e56e2f
Steven Alford <steven.alford@collinsonlatitude.com>
2014-09-24 21:24:45 +0800

    More Control

--- Assets/Dude.cs
+++ Assets/Dude.cs
@@ -6,25 +6,24 @@ public class Dude : MonoBehaviour
     Animator animatorThing;
     public Camera ChaseCamera;
 
+    private Vector2 facing;
+
     // Use this for initialization
     void Start()
     {
         animatorThing = GetComponent<Animator>();
-
-
+        facing = new Vector2(0, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        animatorThing.SetFloat("Speed", Input.GetAxis("Vertical"));
+        facing = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        animatorThing.SetFloat("Speed", facing.normalized.magnitude);
 
-        //if (Input.GetAxis("Vertical") > 0)
-        //{ 
-        //}
-        //if (Input.GetAxis("Vertical") < 0)
-        //{
-        //}
+        var targetYaw = Mathf.Atan2(facing.x, facing.y)*Mathf.Rad2Deg;
+        Debug.Log(facing + " - " + targetYaw);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, targetYaw, transform.eulerAngles.z), Time.deltaTime);
 
         ChaseCamera.transform.position = transform.position + new Vector3(0, 7.5f, -7.5f);
         ChaseCamera.transform.LookAt(transform.position);
